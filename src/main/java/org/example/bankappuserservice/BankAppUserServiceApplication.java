f8992744085bdfd2668f0c32520083325c50d9d9
ValmirboJr <valmirborgesojr@gmail.com>
2026-08-16 23:43:15 -0300

    first commit

---
+++ src/main/java/org/example/bankappuserservice/BankAppUserServiceApplication.java
@@ -0,0 +1,13 @@
+package org.example.bankappuserservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BankAppUserServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BankAppUserServiceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/example/bankappuserservice/BankAppUserServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.bankappuserservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BankAppUserServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
