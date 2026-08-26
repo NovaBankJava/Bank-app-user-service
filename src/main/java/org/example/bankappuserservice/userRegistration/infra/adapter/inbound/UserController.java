24df448ad81fe817cb9a1532dc23e41a81c74eea
Matheus Henrique Barros Russo dos Santos <matheusrusso@MacBook-Air-de-Matheus.local>
2026-08-25 21:17:12 -0300

    feat: Cria a controller de Usuario

---
+++ src/main/java/org/example/bankappuserservice/userRegistration/infra/adapter/inbound/UserController.java
@@ -0,0 +1,27 @@
+package org.example.bankappuserservice.userRegistration.infra.adapter.inbound;
+
+import org.example.bankappuserservice.userRegistration.domain.ports.in.CreateUserInput;
+import org.example.bankappuserservice.userRegistration.domain.ports.in.CreateUserUseCase;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/register")
+public class UserController {
+            private final CreateUserUseCase createUserUseCase;
+
+    public UserController(CreateUserUseCase createUserUseCase) {
+        this.createUserUseCase = createUserUseCase;
+    }
+
+    @PostMapping
+    public ResponseEntity<Void> createUser(@RequestBody CreateUserInput input){
+        createUserUseCase.execute(input);
+        return new ResponseEntity<>(HttpStatus.CREATED);
+    }
+
+}
