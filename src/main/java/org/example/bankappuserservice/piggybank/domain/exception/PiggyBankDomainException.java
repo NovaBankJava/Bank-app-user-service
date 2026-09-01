fa18fc01a2abf254ae1f17afe530e1963d0bc729
MathSMB <matheusbrederpuc@gmail.com>
2026-08-31 22:58:08 -0300

    wip(piggybank): adicionando a classe de excessao PiggyBankDomainException

---
+++ src/main/java/org/example/bankappuserservice/piggybank/domain/exception/PiggyBankDomainException.java
@@ -0,0 +1,7 @@
+package org.example.bankappuserservice.piggybank.domain.exception;
+
+public class PiggyBankDomainException extends RuntimeException {
+    public PiggyBankDomainException(String message) {
+        super(message);
+    }
+}
